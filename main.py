41a0e55139caf63c4f963f79148c214403a842d7
dklopstein <dklopstein@ucsd.edu>
2025-08-19 19:51:48 -0700

    used SQLModel with Pydantic to validate data; used asyncio + aiohttp to make concurrent API calls to add 3 cities at once to SQLite db

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 import os
 from dotenv import load_dotenv
-import requests
-from pydantic import BaseModel, field_validator
+import asyncio
+import aiohttp
+from pydantic import field_validator
 from datetime import datetime
 from sqlmodel import SQLModel, Field, create_engine, Session
 from typing import Optional
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Access the API key
-api_key = os.getenv('API_KEY')
-
-# URL for Tomorrow.io Realtime Weather API
-url = f'https://api.tomorrow.io/v4/weather/realtime?location=93312%20US&units=imperial&apikey={api_key}'
-
-headers = {
-    'accept': 'application/json',
-    'accept-encoding': 'deflate, gzip, br'
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    print('Successfully retrieved weather data.')
-else:
-    print(f'Error: {response.status_code} - {response.text}')
-
+# Create table models for the data received from the API
 # Flatten the JSON response for easier handling
 def json_to_weather_db(json_data):
     """Convert Weather JSON data to WeatherDB"""
@@ -85,10 +66,45 @@ engine = create_engine('sqlite:///weather.db')
 # Create table in SQLite if it doesn't exist
 SQLModel.metadata.create_all(engine)
 
-# Convert JSON to WeatherDB model
-weather_entry = json_to_weather_db(response.json())
 
-# Insert into SQLite
-with Session(engine) as session:
-    session.add(weather_entry)
-    session.commit()
+# Handle concurrent API calls
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the API key
+api_key = os.getenv('API_KEY')
+
+# URLs for Tomorrow.io Realtime Weather API
+locations = [
+    '93312%20US', # Bakersfield, CA, USA
+    '93106%20US', # UCSB
+    '92093%20US'  # UCSD
+    ]
+
+urls = [f'https://api.tomorrow.io/v4/weather/realtime?location={location}&units=imperial&apikey={api_key}' for location in locations]
+
+# Run API requests concurrently
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.json()
+
+async def fetch_all(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(session, url) for url in urls]
+        results = await asyncio.gather(*tasks)
+        return results
+    
+async def main():
+    # Fetch concurrently
+    json_data = await fetch_all(urls)
+
+    # Convert JSON to WeatherDB models
+    weather_entries = [json_to_weather_db(data) for data in json_data]
+
+    # Insert into SQLite
+    with Session(engine) as session:
+        session.add_all(weather_entries)
+        session.commit()
+
+if __name__ == "__main__":
+    results = asyncio.run(main())
